17f876c15960d60f8c7097e01eeb2148b31c3e1a
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2021-10-25 13:07:12 -0400

    Updated CI to push ghcr & version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'mgz2imageslices',
-    version          = '2.0.10',
+    version          = '2.1.0',
     description      = 'An app to convert 3d mgz files to 2s slices of readable formats like PNG/JPEG',
     long_description = readme,
     author           = 'Arushi Vyas',
